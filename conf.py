b37428fb78c9e3f8f1334cbcd2ab525172574186
momo <1668817352@qq.com>
2025-03-17 15:53:54 +0800

    txtxtxt

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@ author = 'mm'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst _parser","sphinx_copybutton"]
+extensions = ["myst_parser","sphinx_copybutton"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
